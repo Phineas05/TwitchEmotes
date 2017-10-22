6878c399f04acb39351bde85cb4869785ea718b6
Phineas <Phineas05@users.noreply.github.com>
2017-10-22 20:01:34 +0530

    Added more emotes

--- replacer.js
+++ replacer.js
@@ -15,6 +15,7 @@ function onLoad() {
 		"BrokeBack": "https://static-cdn.jtvnw.net/emoticons/v1/4057/1.0",
 		"CoolCat": "https://static-cdn.jtvnw.net/emoticons/v1/58127/1.0",
 		"CoolStoryBob": "https://static-cdn.jtvnw.net/emoticons/v1/123171/1.0",
+		"cmonBruh": "https://static-cdn.jtvnw.net/emoticons/v1/84608/1.0",
 		"CurseLit": "https://static-cdn.jtvnw.net/emoticons/v1/116625/1.0",
 		"DansGame": "https://static-cdn.jtvnw.net/emoticons/v1/33/1.0",
 		"DatSheffy": "https://static-cdn.jtvnw.net/emoticons/v1/111700/1.0",
@@ -25,8 +26,28 @@ function onLoad() {
 		"HeyGuys": "https://static-cdn.jtvnw.net/emoticons/v1/30259/1.0",
 		"HotPokket": "https://static-cdn.jtvnw.net/emoticons/v1/357/1.0",
 		"Jebaited": "https://static-cdn.jtvnw.net/emoticons/v1/114836/1.0",
-		"Kappa": "https://static-cdn.jtvnw.net/emoticons/v1/25/1.0",
 		"KappaPride": "https://static-cdn.jtvnw.net/emoticons/v1/55338/1.0",
+		"Kappa": "https://static-cdn.jtvnw.net/emoticons/v1/25/1.0",
+		"Keepo": "https://static-cdn.jtvnw.net/emoticons/v1/1902/1.0",
+		"KreyGasm": "https://static-cdn.jtvnw.net/emoticons/v1/41/1.0",
+		"LUL": "https://static-cdn.jtvnw.net/emoticons/v1/425618/1.0",
+		"MingLee": "https://static-cdn.jtvnw.net/emoticons/v1/68856/1.0",
+		"MrDestructoid": "https://static-cdn.jtvnw.net/emoticons/v1/28/1.0",
+		"NotLikeThis": "https://static-cdn.jtvnw.net/emoticons/v1/58765/1.0",
+		"OpieOP": "https://static-cdn.jtvnw.net/emoticons/v1/100590/1.0",
+		"PJSalt": "https://static-cdn.jtvnw.net/emoticons/v1/36/1.0",
+		"PogChamp": "https://static-cdn.jtvnw.net/emoticons/v1/88/1.0",
+		"PunchTrees": "https://static-cdn.jtvnw.net/emoticons/v1/47/1.0",
+		"ResidentSleeper": "https://static-cdn.jtvnw.net/emoticons/v1/245/1.0",
+		"SMOrc": "https://static-cdn.jtvnw.net/emoticons/v1/52/1.0",
+		"SSSsss": "https://static-cdn.jtvnw.net/emoticons/v1/46/1.0",
+		"SeemsGood": "https://static-cdn.jtvnw.net/emoticons/v1/64138/1.0",
+		"SwiftRage": "https://static-cdn.jtvnw.net/emoticons/v1/34/1.0",
+		"TTours": "https://static-cdn.jtvnw.net/emoticons/v1/38436/1.0",
+		"TriHard": "https://static-cdn.jtvnw.net/emoticons/v1/120232/1.0",
+		"VoteNay": "https://static-cdn.jtvnw.net/emoticons/v1/106294/1.0",
+		"VoteYea": "https://static-cdn.jtvnw.net/emoticons/v1/106293/1.0",
+		"WutFace": "https://static-cdn.jtvnw.net/emoticons/v1/28087/1.0"
 	};
 	for (var emoteName in emoteList) {
 		emoteList[emoteName] = "<img src=\"" + emoteList[emoteName] + "\"\\>";
@@ -45,7 +66,7 @@ function replacePhrasesWithEmotes(element, elementTagName, elementContent) {
 		for (var emoteName in emoteList) {
 			var emoteImg = emoteList[emoteName];
 			var regExp = new RegExp("\\b" + emoteName + "\\b", "gi");
-			elementContent = elementContent.replace(regExp, emoteImg);		
+			elementContent = elementContent.replace(regExp, emoteImg);
 		}
 		element.html(elementContent);
 	}
