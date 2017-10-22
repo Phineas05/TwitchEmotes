6ba3d2f0ccfa1fb36901a91300a0ada0a291e191
Phineas <Phineas05@users.noreply.github.com>
2017-10-22 19:32:13 +0530

    Fixed YouTube breaking

--- replacer.js
+++ replacer.js
@@ -3,17 +3,22 @@ var blacklistedTags;
 var emoteList;
 
 function onLoad() {
-	blacklistedTags = ["TITLE", "STYLE", "SCRIPT", "IMG"];
+	blacklistedTags = ["TITLE", "STYLE", "LINK", "TEMPLATE", "SCRIPT", "IMG"];
 	emoteList = {
 		"kappa": "<img src=\"https://static-cdn.jtvnw.net/emoticons/v1/25/1.0\"\\>"
 	};
-	$("*:not(:has(*))").each(function() {
+	startReplaceLoop();
+}
+
+function startReplaceLoop() {
+	$("body *:not(:has(*)), h1").each(function() {
 		replacePhrasesWithEmotes($(this), $(this).prop("tagName"), $(this).html());
 	});
 }
 
 function replacePhrasesWithEmotes(element, elementTagName, elementContent) {
 	if (blacklistedTags.indexOf(elementTagName) < 0) {
+		console.log(elementTagName);
 		for (var oldText1 in emoteList) {
 			var oldText2 = oldText1.charAt(0).toUpperCase() + oldText1.slice(1);
 			var oldText3 = oldText1.toUpperCase();
@@ -23,9 +28,6 @@ function replacePhrasesWithEmotes(element, elementTagName, elementContent) {
 			var regExp1 = new RegExp("\\b" + oldText1 + "\\b", "g");
 			var regExp2 = new RegExp("\\b" + oldText2 + "\\b", "g");
 			var regExp3 = new RegExp("\\b" + oldText3 + "\\b", "g");
-			console.log(elementContent.match(regExp1));
-			console.log(elementContent.match(regExp2));
-			console.log(elementContent.match(regExp3));
 			elementContent = elementContent.replace(regExp1, newText1);
 			elementContent = elementContent.replace(regExp2, newText2);
 			elementContent = elementContent.replace(regExp3, newText3);			
