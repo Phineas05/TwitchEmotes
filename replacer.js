4ab234aeb9b2835e1b0fe64cbb21d706729dc352
Phineas <Phineas05@users.noreply.github.com>
2017-10-22 19:44:59 +0530

    Added a few emotes

--- replacer.js
+++ replacer.js
@@ -5,8 +5,32 @@ var emoteList;
 function onLoad() {
 	blacklistedTags = ["TITLE", "STYLE", "LINK", "TEMPLATE", "SCRIPT", "IMG"];
 	emoteList = {
-		"kappa": "<img src=\"https://static-cdn.jtvnw.net/emoticons/v1/25/1.0\"\\>"
+		"4Head": "https://static-cdn.jtvnw.net/emoticons/v1/354/1.0",
+		"ANELE": "https://static-cdn.jtvnw.net/emoticons/v1/3792/1.0",
+		"BabyRage": "https://static-cdn.jtvnw.net/emoticons/v1/22639/1.0",
+		"BibleThump": "https://static-cdn.jtvnw.net/emoticons/v1/86/1.0",
+		"BigBrother": "https://static-cdn.jtvnw.net/emoticons/v1/1904/1.0",
+		"Kappa": "https://static-cdn.jtvnw.net/emoticons/v1/25/1.0",
+		"BlessRNG": "https://static-cdn.jtvnw.net/emoticons/v1/153556/1.0",
+		"BrokeBack": "https://static-cdn.jtvnw.net/emoticons/v1/4057/1.0",
+		"CoolCat": "https://static-cdn.jtvnw.net/emoticons/v1/58127/1.0",
+		"CoolStoryBob": "https://static-cdn.jtvnw.net/emoticons/v1/123171/1.0",
+		"CurseLit": "https://static-cdn.jtvnw.net/emoticons/v1/116625/1.0",
+		"DansGame": "https://static-cdn.jtvnw.net/emoticons/v1/33/1.0",
+		"DatSheffy": "https://static-cdn.jtvnw.net/emoticons/v1/111700/1.0",
+		"DendeiFace": "https://static-cdn.jtvnw.net/emoticons/v1/58135/1.0",
+		"EleGiggle": "https://static-cdn.jtvnw.net/emoticons/v1/4339/1.0",
+		"FailFish": "https://static-cdn.jtvnw.net/emoticons/v1/360/1.0",
+		"FrankerZ": "https://static-cdn.jtvnw.net/emoticons/v1/65/1.0",
+		"HeyGuys": "https://static-cdn.jtvnw.net/emoticons/v1/30259/1.0",
+		"HotPokket": "https://static-cdn.jtvnw.net/emoticons/v1/357/1.0",
+		"Jebaited": "https://static-cdn.jtvnw.net/emoticons/v1/114836/1.0",
+		"Kappa": "https://static-cdn.jtvnw.net/emoticons/v1/25/1.0",
+		"KappaPride": "https://static-cdn.jtvnw.net/emoticons/v1/55338/1.0",
 	};
+	for (var emoteName in emoteList) {
+		emoteList[emoteName] = "<img src=\"" + emoteList[emoteName] + "\"\\>";
+	}
 	startReplaceLoop();
 }
 
@@ -18,18 +42,10 @@ function startReplaceLoop() {
 
 function replacePhrasesWithEmotes(element, elementTagName, elementContent) {
 	if (blacklistedTags.indexOf(elementTagName) < 0) {
-		for (var oldText1 in emoteList) {
-			var oldText2 = oldText1.charAt(0).toUpperCase() + oldText1.slice(1);
-			var oldText3 = oldText1.toUpperCase();
-			var newText1 = emoteList[oldText1];
-			var newText2 = newText1.charAt(0).toUpperCase() + newText1.slice(1);
-			var newText3 = newText1.toUpperCase();
-			var regExp1 = new RegExp("\\b" + oldText1 + "\\b", "g");
-			var regExp2 = new RegExp("\\b" + oldText2 + "\\b", "g");
-			var regExp3 = new RegExp("\\b" + oldText3 + "\\b", "g");
-			elementContent = elementContent.replace(regExp1, newText1);
-			elementContent = elementContent.replace(regExp2, newText2);
-			elementContent = elementContent.replace(regExp3, newText3);			
+		for (var emoteName in emoteList) {
+			var emoteImg = emoteList[emoteName];
+			var regExp = new RegExp("\\b" + emoteName + "\\b", "gi");
+			elementContent = elementContent.replace(regExp, emoteImg);		
 		}
 		element.html(elementContent);
 	}
